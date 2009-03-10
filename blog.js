cc2385ba918299d75970902f4cadc95eb3aa7b5d
Eliot Horowitz <eliot@10gen.com>
2009-03-10 18:10:44 -0400

    blog snippets

---
+++ blog.js
@@ -0,0 +1,70 @@
+
+// clean up from previous runs
+db.posts.drop();
+
+db.posts.save( 
+    { 
+        title : "welcome to my blog" ,
+        text : "this is my first entry, yay!" ,
+        posted : new Date( ( new Date() ).getTime() - ( 1000 * 3600 ) ) ,
+        tags : [ "admin" , "fun" ]
+    }
+);
+
+
+db.posts.save( 
+    { 
+        title : "i think mongo is cool" ,
+        text : "mongo is fast and not relational" ,
+        posted : new Date() ,
+        tags : [ "mongo" , "fun" ]
+    }
+);
+
+
+db.posts.ensureIndex( { title : 1 } ); // for looking up by title
+db.posts.ensureIndex( { posted : 1 } ); // for sorting so you can see the newest
+db.posts.ensureIndex( { tags : 1 } ); // so you can search for tags
+
+db.posts.find().forEach( printjson ); // printjson is a utility function we have
+
+print( db.posts.findOne( { tags : "admin" } ).title ); // if you index an array, you can search for elements in it
+print( db.posts.findOne( { tags : "mongo" } ).title );
+
+
+// for computing things like tag clouds, you can run javascript in the db
+function tagCloud(){
+
+    var counts = db.eval(
+        function(){
+            var counts = {};
+            db.posts.find().forEach(
+                function(p){
+                    if ( p.tags ){
+                        for ( var i=0; i<p.tags.length; i++ ){
+                            var name = p.tags[i];
+                            
+                            counts[name] = 1 + ( counts[name] || 0 );
+                        }
+                    }
+                }
+            );
+            return counts;
+        }
+    );
+    
+    // maybe sort to by nice
+    var sorted = [];
+    for ( var tag in counts ){
+        sorted.push( { name : tag , num : counts[tag] } )
+    }
+    
+    return sorted.sort(
+        function(l,r){
+            return r.num - l.num;
+        }
+    );
+    
+}
+
+printjson( tagCloud() );
