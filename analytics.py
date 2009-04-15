0025f898ba96ac45d79e5d310faea50ada258b26
Eliot Horowitz <eliot@10gen.com>
2009-04-15 10:22:19 -0400

    python analytics example

---
+++ analytics.py
@@ -0,0 +1,17 @@
+# doing real time couting with pymongo
+
+from pymongo.connection import Connection
+import datetime
+
+def now( cur = datetime.datetime.utcnow() ):
+    return cur.replace(minute=cur.minute - cur.minute % 1, second=0, microsecond=0 )
+
+connection = Connection()
+db = connection["analytics_sample"]
+db.drop_collection( "my_analytics" )
+
+db.my_analytics.update( { "page" : "/" } , { "$inc" : { "count" : 1 } } , upsert=True )
+db.my_analytics.update( { "page" : "/" } , { "$inc" : { "count" : 1 } } , upsert=True )
+
+print( db.my_analytics.find_one() )
+print( db.my_analytics.count() )
