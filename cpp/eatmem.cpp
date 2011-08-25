47a9ae9797a4b2ad1964b716d240a6a696e30b45
dwight <dwight@10gen.com>
2011-08-24 20:04:43 -0400

    eatmem

---
+++ cpp/eatmem.cpp
@@ -0,0 +1,37 @@
+// g++ eatmem.cpp
+
+#include <iostream>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <errno.h>
+
+using namespace std;
+
+int main(int argc, char *argv[]) { 
+  if( argc != 2 ) {
+    cout << "usage: eatmem <MB>" << endl;
+    return 1;
+  }
+  int mb = atoi(argv[1]);
+  cout << "reserving " << mb << "MB of RAM" << endl;
+  void *p = malloc(mb * 1024 * 1024);
+  if( p == 0 ) { 
+    cout << "malloc failed" << endl;
+    return 2;
+  }
+
+  if( mlockall(MCL_CURRENT) ) { 
+    cout << "mlockall failed errno:" << errno << endl;
+    cout << "use sudo if you did not" << endl;
+    return 3;
+  }
+
+  cout << "reserved ok, sleeping" << endl;
+  while( 1 ) { 
+    sleep(1024);
+  }
+
+  return 0;
+}
