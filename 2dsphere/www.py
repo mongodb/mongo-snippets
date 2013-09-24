ac1222d03e408d57fa3f774d9a1d80f48099014c
Samantha Ritter <samantha.ritter@10gen.com>
2013-09-24 17:47:47 -0400

    change widget to accomodate MultiPolygons and GeometryCollections

--- 2dsphere/www.py
+++ 2dsphere/www.py
@@ -18,6 +18,16 @@ db = conn[geo_collection['db_name']]
 
 db.write_concern = {'w': 1}
 
+def is_polygon(list):
+    n = len(list)
+    if n < 4:
+        return False
+    elif list[0][0] != list[n - 1][0]:
+            return False
+    elif list[0][1] != list[n - 1][1]:
+        return False
+    return True
+
 class GeoDemo(object):
 
     def GET(self):
@@ -30,38 +40,67 @@ class GeoSearch(object):
         collection = db[geo_collection['collection_name']]
         # Web.py doesn't support arrays of objects, so we use two 
         # arrays, one with lats, one with lngs.
-        input = web.input(point_lats=[], point_lngs=[])
+
+        input = web.input(point_lats=[], point_lngs=[]) # must fix...
         mode = input['mode']
+        path_count = int(input['path_count'])
+        
         point_lats = input['point_lats']
-        point_lngs = input['point_lngs']
+        point_lngs = input['point_lngs']    
+    
         final_points = []
-        for i in range(0, len(point_lats)):
-            final_points.append([float(point_lngs[i]), float(point_lats[i])])
+        a = 0
+        for j in range(0, path_count):
+
+            length = int(input['length_' + str(j)])
+            final_points.append([])
+            for i in range(0, length):
+                final_points[j].append([float(point_lngs[a]), float(point_lats[a])])
+                a += 1
+
         # don't bring back _id, because it makes json encoding uglier.
         if mode == "$within":
-            final_points.append(final_points[0])
+            for i in range(0, path_count):
+                final_points[i] = [final_points[i]]
             poly = {
-                "type": "Polygon",
-                "coordinates": [final_points]
+                "type": "MultiPolygon",
+                "coordinates": final_points
             }
-            within = collection.find({"geo": { "$within": {"$geometry": poly} } }, 
-                    {"_id": False, "name": True, "geo": True})
+
+            within = collection.find({"geo": { "$geoWithin": {"$geometry": poly} } }, 
+                                     {"_id": False, "name": True, "geo": True})
             within.limit(500)
             places = [place for place in within]
         elif mode == "$geoIntersects":
-            line = {
-                    "type": "LineString",
-                    "coordinates": final_points
+            # step 1: make everything a geo collection
+            geometries = []
+            for i in range(0, path_count):
+                doc = {}
+                if len(final_points[i]) == 1:
+                    doc["type"] = "Point"
+                    doc["coordinates"] = final_points[i][0]
+                elif is_polygon(final_points[i]):
+                    doc["type"] = "Polygon"
+                    doc["coordinates"] = [final_points[i]]
+                else:
+                    doc["type"] = "LineString"
+                    doc["coordinates"] = final_points[i]
+                geometries.append(doc)
+            # step 2: add logic to find things that are all one type
+            # step 3: make multipoint, multiline, multigon docs.
+            coll = {
+                    "type": "GeometryCollection",
+                    "geometries": geometries
                     }
-            inter = collection.find({"geo": { "$geoIntersects": {"$geometry": line} } },
+            inter = collection.find({"geo": { "$geoIntersects": {"$geometry": coll} } },
                     {"_id": False, "name": True, "geo": True})
             inter.limit(50)
-            print line
+#            print geometries
             places = [place for place in inter]
         elif mode == "$near":
             point = {
                     "type": "Point",
-                    "coordinates": final_points[0]
+                    "coordinates": final_points[0][0]
                     }
             near = collection.find({"geo": { "$near": {"$geometry": point} } }, 
                     {"_id": False, "name": True, "geo": True})
